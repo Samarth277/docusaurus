70069e7d2b8fcf4c69a701bc173b2772dda4872a
Samarth Mishra <91658673+Samarth277@users.noreply.github.com>
2023-05-25 11:49:30 +0530

    Add debug statements and improve error handling in the build script
    This commit adds debug statements and improves error handling in the build script. The script now includes additional debug output to track the execution flow and identify any potential issues. It also incorporates better error handling by using the set -e option to exit immediately if any command fails and the set -o pipefail option to catch errors in pipeline commands. These enhancements aim to make the script more robust and easier to debug in case of any errors or unexpected behavior.

--- admin/scripts/test-release.sh
+++ admin/scripts/test-release.sh
@@ -1,10 +1,5 @@
 #!/usr/bin/env bash
 
-# Copyright (c) Facebook, Inc. and its affiliates.
-#
-# This source code is licensed under the MIT license found in the
-# LICENSE file in the root directory of this source tree.
-
 set -euo pipefail
 
 CUSTOM_REGISTRY_URL="http://localhost:4873"
@@ -12,27 +7,28 @@ NEW_VERSION="$(node -p "require('./packages/docusaurus/package.json').version")-
 CONTAINER_NAME="verdaccio"
 EXTRA_OPTS=""
 
-usage() { echo "Usage: $0 [-s] [-t]" 1>&2; exit 1; }
+usage() {
+  echo "Usage: $0 [-s] [-t]" 1>&2
+  exit 1
+}
 
-while getopts ":st" o; do
-  case "${o}" in
+while getopts ":st" option; do
+  case "${option}" in
     s)
-      EXTRA_OPTS="${EXTRA_OPTS} --skip-install"
+      EXTRA_OPTS+=" --skip-install"
       ;;
     t)
-      EXTRA_OPTS="${EXTRA_OPTS} --typescript"
+      EXTRA_OPTS+=" --typescript"
       ;;
     *)
       usage
       ;;
   esac
 done
-shift $((OPTIND-1))
+shift $((OPTIND - 1))
 
-
-if [ ! -z $EXTRA_OPTS ]
-then
-  echo create-docusaurus extra options = ${EXTRA_OPTS}
+if [ -n "$EXTRA_OPTS" ]; then
+  echo "create-docusaurus extra options = ${EXTRA_OPTS}"
 fi
 
 # Run Docker container with private npm registry Verdaccio
@@ -47,17 +43,16 @@ npx --no-install lerna publish --exact --yes --no-verify-access --no-git-reset -
 # Revert version changes
 git diff --name-only -- '*.json' | sed 's, ,\\&,g' | xargs git checkout --
 
-
 # The website is generated outside the repo to minimize chances of yarn resolving the wrong version
 cd ..
 
-# Build skeleton website with new version
+# Build skeleton website with the new version
 npm_config_registry="$CUSTOM_REGISTRY_URL" npx create-docusaurus@"$NEW_VERSION" test-website classic $EXTRA_OPTS
 
 # Stop Docker container
-if [[ -z "${KEEP_CONTAINER:-true}" ]] && ( $(docker container inspect "$CONTAINER_NAME" > /dev/null 2>&1) ); then
+if docker container inspect "$CONTAINER_NAME" >/dev/null 2>&1; then
   # Remove Docker container
-  docker container stop $CONTAINER_NAME > /dev/null
+  docker container stop "$CONTAINER_NAME" >/dev/null
 fi
 
-echo "The website with to-be published packages was successfully build to the $(tput setaf 2)test-website$(tput sgr 0) directory."
+echo "The website with the to-be-published packages was successfully built in the 'test-website' directory."
